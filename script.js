48a0257ebe735d24480b5b00e56a143cd437400b
Ljohn10 <eljohn.barrer@gmail.com>
2023-04-22 16:11:43 +0800

    First commit

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ darkModeIcon.onclick = () => {
 
 /*=====Scroll Reveal====*/
 ScrollReveal({ 
-    //reset: true,
+    reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200
@@ -78,5 +78,4 @@ ScrollReveal({
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, testimonial-wrapper, contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
-
 ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
